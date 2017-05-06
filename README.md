76e2c50d4dced62d9f712998813d09ed7132750e
weis <weis.tobi@googlemail.com>
2017-05-07 01:10:25 +0200

    structure

--- README.md
+++ README.md
@@ -4,3 +4,16 @@ A robot that is controlled by python on a raspberry pi.
 The build will be rover-like, with four driven wheels.
 
 ![PyPiBot](0001.png)
+
+## Parts-List
+Raspberry-Pi B+ V2
+Edimax EW-7811UN Wireless USB Adapter
+
+Motors: 4x DC 6.0V, 35:1 gear ratio, diameter of shaft: 4mm
+Motor-mounts: 4x DROK 25mm universal dc motor mount
+Motor-driver: 2x L298 dual H-bridge modules
+
+Tires: 4x GoolRC 1:10 Drift Car Tires
+Tire-mount: 12mm Hex Wheel Adapter for 4mm shaft
+
+
---
+++ code/prototypes/gpio_motor.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+import time
+import RPi.GPIO as GPIO
+
+M1A = 3
+M1B = 5
+M1E = 7
+
+GPIO.setwarnings(False)
+
+GPIO.setmode(GPIO.BOARD)
+
+GPIO.setup(M1A, GPIO.OUT)
+GPIO.setup(M1B, GPIO.OUT)
+GPIO.setup(M1E, GPIO.OUT)
+
+GPIO.output(3, GPIO.LOW)
+GPIO.output(5, GPIO.HIGH)
+p = GPIO.PWM(M1E, 100)
+p.start(0)
+
+for i in range(10,101,10):
+    print i
+    p.ChangeDutyCycle(i)
+    time.sleep(5)
+
+GPIO.output(3, GPIO.LOW)
+GPIO.output(5, GPIO.LOW)
+
+GPIO.clenaup()
