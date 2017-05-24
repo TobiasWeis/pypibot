634a47060e39fcf259f3ea2acc620fb0765c3fc0
weis <weis.tobi@googlemail.com>
2017-05-25 00:34:02 +0200

    added dummy classes to run even if not on RPi

--- code/framework/modules/MOT.py
+++ code/framework/modules/MOT.py
@@ -1,5 +1,4 @@
 from MP import MP
-import RPi.GPIO as GPIO
 from utils import *
 import math
 import time
@@ -11,198 +10,287 @@ move commands like
 move(angle, distance)
 '''
 
-class Motor():
-    def __init__(self,a,b,e):
-        self.a = a
-        self.b = b
-        self.e = e
 
-        GPIO.setup(self.a, GPIO.OUT)
-        GPIO.setup(self.b, GPIO.OUT)
-        GPIO.setup(self.e, GPIO.OUT)
+try:
+    import RPi.GPIO as GPIO
 
-        self.set_mode("release")
+    class Motor():
+        def __init__(self,a,b,e):
+            self.a = a
+            self.b = b
+            self.e = e
 
-        # setup PWM on enable port
-        self.p = GPIO.PWM(self.e, 100)
-        self.p.start(0)
+            GPIO.setup(self.a, GPIO.OUT)
+            GPIO.setup(self.b, GPIO.OUT)
+            GPIO.setup(self.e, GPIO.OUT)
 
-    def set_mode(self, d):
-        if d == "backward":
-            GPIO.output(self.a, GPIO.HIGH)
-            GPIO.output(self.b, GPIO.LOW)
-        elif d == "forward":
-            GPIO.output(self.a, GPIO.LOW)
-            GPIO.output(self.b, GPIO.HIGH)
-        elif d == "block":
-            GPIO.output(self.a, GPIO.HIGH)
-            GPIO.output(self.b, GPIO.HIGH)
-        elif d == "release":
-            GPIO.output(self.a, GPIO.LOW)
-            GPIO.output(self.b, GPIO.LOW)
-        else:
-            print "Motor: I do not know this direction"
+            self.set_mode("release")
 
-    def set_speed(self,speed):
-        self.p.ChangeDutyCycle(speed)
+            # setup PWM on enable port
+            self.p = GPIO.PWM(self.e, 100)
+            self.p.start(0)
 
-    def cleanup(self):
-        self.set_speed(0)
-        self.set_mode("release")
+        def set_mode(self, d):
+            if d == "backward":
+                GPIO.output(self.a, GPIO.HIGH)
+                GPIO.output(self.b, GPIO.LOW)
+            elif d == "forward":
+                GPIO.output(self.a, GPIO.LOW)
+                GPIO.output(self.b, GPIO.HIGH)
+            elif d == "block":
+                GPIO.output(self.a, GPIO.HIGH)
+                GPIO.output(self.b, GPIO.HIGH)
+            elif d == "release":
+                GPIO.output(self.a, GPIO.LOW)
+                GPIO.output(self.b, GPIO.LOW)
+            else:
+                print "Motor: I do not know this direction"
 
-class MOT(MP):
-    def init(self):
-        self.motion = None
-	self.mcs = Coordinate()
-        self.mcs_t = getMs()
+        def set_speed(self,speed):
+            self.p.ChangeDutyCycle(speed)
 
-        GPIO.setwarnings(False)
-        GPIO.setmode(GPIO.BOARD)
+        def cleanup(self):
+            self.set_speed(0)
+            self.set_mode("release")
 
-        self.motor1 = Motor(
-                        self.config.getint("PINS", "MOTOR1A"),
-                        self.config.getint("PINS", "MOTOR1B"),
-                        self.config.getint("PINS", "MOTOR1E")
-                        )
+    class MOT(MP):
+        def init(self):
+            self.motion = None
+            self.mcs = Coordinate()
+            self.mcs_t = getMs()
 
-        self.motor2 = Motor(
-                        self.config.getint("PINS", "MOTOR2A"),
-                        self.config.getint("PINS", "MOTOR2B"),
-                        self.config.getint("PINS", "MOTOR2E")
-                        )
+            GPIO.setwarnings(False)
+            GPIO.setmode(GPIO.BOARD)
 
-        self.motor3 = Motor(
-                        self.config.getint("PINS", "MOTOR3A"),
-                        self.config.getint("PINS", "MOTOR3B"),
-                        self.config.getint("PINS", "MOTOR3E")
-                        )
+            self.motor1 = Motor(
+                            self.config.getint("PINS", "MOTOR1A"),
+                            self.config.getint("PINS", "MOTOR1B"),
+                            self.config.getint("PINS", "MOTOR1E")
+                            )
 
-        self.motor4 = Motor(
-                        self.config.getint("PINS", "MOTOR4A"),
-                        self.config.getint("PINS", "MOTOR4B"),
-                        self.config.getint("PINS", "MOTOR4E")
-                        )
+            self.motor2 = Motor(
+                            self.config.getint("PINS", "MOTOR2A"),
+                            self.config.getint("PINS", "MOTOR2B"),
+                            self.config.getint("PINS", "MOTOR2E")
+                            )
 
-    def fwd(self,speed):
-        self.motor1.set_mode("forward")
-        self.motor2.set_mode("forward")
-        self.motor3.set_mode("forward")
-        self.motor4.set_mode("forward")
+            self.motor3 = Motor(
+                            self.config.getint("PINS", "MOTOR3A"),
+                            self.config.getint("PINS", "MOTOR3B"),
+                            self.config.getint("PINS", "MOTOR3E")
+                            )
 
-        self.motor1.set_speed(speed)
-        self.motor2.set_speed(speed)
-        self.motor3.set_speed(speed)
-        self.motor4.set_speed(speed)
+            self.motor4 = Motor(
+                            self.config.getint("PINS", "MOTOR4A"),
+                            self.config.getint("PINS", "MOTOR4B"),
+                            self.config.getint("PINS", "MOTOR4E")
+                            )
 
-        self.motion = "forward"
+        def fwd(self,speed):
+            self.motor1.set_mode("forward")
+            self.motor2.set_mode("forward")
+            self.motor3.set_mode("forward")
+            self.motor4.set_mode("forward")
 
-    def bwd(self, speed):
-        self.motor1.set_mode("backward")
-        self.motor2.set_mode("backward")
-        self.motor3.set_mode("backward")
-        self.motor4.set_mode("backward")
+            self.motor1.set_speed(speed)
+            self.motor2.set_speed(speed)
+            self.motor3.set_speed(speed)
+            self.motor4.set_speed(speed)
 
-        self.motor1.set_speed(speed)
-        self.motor2.set_speed(speed)
-        self.motor3.set_speed(speed)
-        self.motor4.set_speed(speed)
+            self.motion = "forward"
 
-        self.motion = "bacward"
+        def bwd(self, speed):
+            self.motor1.set_mode("backward")
+            self.motor2.set_mode("backward")
+            self.motor3.set_mode("backward")
+            self.motor4.set_mode("backward")
 
-    def left(self,speed):
-        self.motor1.set_mode("forward")
-        self.motor2.set_mode("forward")
-        self.motor3.set_mode("backward")
-        self.motor4.set_mode("backward")
+            self.motor1.set_speed(speed)
+            self.motor2.set_speed(speed)
+            self.motor3.set_speed(speed)
+            self.motor4.set_speed(speed)
 
-        self.motor1.set_speed(speed)
-        self.motor2.set_speed(speed)
-        self.motor3.set_speed(speed)
-        self.motor4.set_speed(speed)
-        
-        self.motion = "left"
+            self.motion = "bacward"
 
-    def right(self,speed):
-        self.motor1.set_mode("backward")
-        self.motor2.set_mode("backward")
-        self.motor3.set_mode("forward")
-        self.motor4.set_mode("forward")
+        def left(self,speed):
+            self.motor1.set_mode("forward")
+            self.motor2.set_mode("forward")
+            self.motor3.set_mode("backward")
+            self.motor4.set_mode("backward")
 
-        self.motor1.set_speed(speed)
-        self.motor2.set_speed(speed)
-        self.motor3.set_speed(speed)
-        self.motor4.set_speed(speed)
-        
-        self.motion = "right"
+            self.motor1.set_speed(speed)
+            self.motor2.set_speed(speed)
+            self.motor3.set_speed(speed)
+            self.motor4.set_speed(speed)
+            
+            self.motion = "left"
 
-    def stop(self):
-        self.motor1.set_speed(0)
-        self.motor2.set_speed(0)
-        self.motor3.set_speed(0)
-        self.motor4.set_speed(0)
+        def right(self,speed):
+            self.motor1.set_mode("backward")
+            self.motor2.set_mode("backward")
+            self.motor3.set_mode("forward")
+            self.motor4.set_mode("forward")
 
-        self.motor1.set_mode("block")
-        self.motor2.set_mode("block")
-        self.motor3.set_mode("block")
-        self.motor4.set_mode("block")
+            self.motor1.set_speed(speed)
+            self.motor2.set_speed(speed)
+            self.motor3.set_speed(speed)
+            self.motor4.set_speed(speed)
+            
+            self.motion = "right"
 
-        self.motion = None
+        def stop(self):
+            self.motor1.set_speed(0)
+            self.motor2.set_speed(0)
+            self.motor3.set_speed(0)
+            self.motor4.set_speed(0)
 
-    def integrate_mcs(self,t):
-	x = self.mcs.x + t.x*math.cos(self.mcs.a) - t.y*math.sin(self.mcs.a)
-	y = self.mcs.y + t.x*math.sin(self.mcs.a) + t.y*math.cos(self.mcs.a)
-	a = self.mcs.a + t.a
-	return Coordinate(x,y,a)
+            self.motor1.set_mode("block")
+            self.motor2.set_mode("block")
+            self.motor3.set_mode("block")
+            self.motor4.set_mode("block")
 
-    def calc_t(self):
-        diff = getMs() - self.mcs_t #-- calculate difference to last timestamp
-	diff_s = diff/1000.
+            self.motion = None
 
-        #-- calculate movement of bot
-	t = Coordinate()
-        if self.motion == "forward":
-            t.x = diff_s * self.config.getfloat("MOT", "MPERS50")
-        elif self.motion == "backward":
-            t.x = -diff_s * self.config.getfloat("MOT", "MPERS50")
-        elif self.motion == "left":
-            t.a = diff_s * self.config.getfloat("MOT", "APERS50")
-	elif self.motion == "right":
-            t.a = -diff_s * self.config.getfloat("MOT", "APERS50")
+        def integrate_mcs(self,t):
+            x = self.mcs.x + t.x*math.cos(self.mcs.a) - t.y*math.sin(self.mcs.a)
+            y = self.mcs.y + t.x*math.sin(self.mcs.a) + t.y*math.cos(self.mcs.a)
+            a = self.mcs.a + t.a
+            return Coordinate(x,y,a)
 
-	return t
-        
+        def calc_t(self):
+            diff = getMs() - self.mcs_t #-- calculate difference to last timestamp
+            diff_s = diff/1000.
 
-    def run_impl(self):
-        # integrate motion to perform dead-reckoning
-        self.mcs = self.integrate_mcs(self.calc_t())
-        self.mcs_t = getMs() #-- get current time in milliseconds
+            #-- calculate movement of bot
+            t = Coordinate()
+            if self.motion == "forward":
+                t.x = diff_s * self.config.getfloat("MOT", "MPERS50")
+            elif self.motion == "backward":
+                t.x = -diff_s * self.config.getfloat("MOT", "MPERS50")
+            elif self.motion == "left":
+                t.a = diff_s * self.config.getfloat("MOT", "APERS50")
+            elif self.motion == "right":
+                t.a = -diff_s * self.config.getfloat("MOT", "APERS50")
 
-        if "Move" in self.md:
-            if len(self.md["Move"]) == 2:
-                #-- get the command
-                print self.name, " - MOVING!"
-                speed = self.md["Move"][0]
-                direction = self.md["Move"][1]
-                #-- take it out of the queue
-                self.md["Move"] = []
-                #-- execute
-                if direction == "forward":
-                    print "Motor: FWD"
-                    self.fwd(speed)
-                elif direction == "backward":
-                    print "Motor: BWD"
-                    self.bwd(speed)
-                elif direction == "left":
-                    print "Motor: LEFT"
-                    self.left(speed)
-                elif direction == "right":
-                    print "Motor: RIGHT"
-                    self.right(speed)
+            return t
+            
 
-                time.sleep(1)
-                self.stop()
-                print self.name, " - MOVING DONE!"
+        def run_impl(self):
+            # integrate motion to perform dead-reckoning
+            self.mcs = self.integrate_mcs(self.calc_t())
+            self.mcs_t = getMs() #-- get current time in milliseconds
 
-    def cleanup(self):
-        self.motor1.cleanup()
-        GPIO.cleanup()
+            if "Move" in self.md:
+                if len(self.md["Move"]) == 2:
+                    #-- get the command
+                    print self.name, " - MOVING!"
+                    speed = self.md["Move"][0]
+                    direction = self.md["Move"][1]
+                    #-- take it out of the queue
+                    self.md["Move"] = []
+                    #-- execute
+                    if direction == "forward":
+                        print "Motor: FWD"
+                        self.fwd(speed)
+                    elif direction == "backward":
+                        print "Motor: BWD"
+                        self.bwd(speed)
+                    elif direction == "left":
+                        print "Motor: LEFT"
+                        self.left(speed)
+                    elif direction == "right":
+                        print "Motor: RIGHT"
+                        self.right(speed)
+
+                    time.sleep(1)
+                    self.stop()
+                    print self.name, " - MOVING DONE!"
+
+        def cleanup(self):
+            self.motor1.cleanup()
+            self.motor2.cleanup()
+            self.motor3.cleanup()
+            self.motor4.cleanup()
+            GPIO.cleanup()
+
+except:
+    class MOT(MP):
+        def init(self):
+            self.motion = None
+            self.mcs = Coordinate()
+            self.mcs_t = getMs()
+
+
+        def fwd(self,speed):
+            self.motion = "forward"
+
+        def bwd(self, speed):
+            self.motion = "bacward"
+
+        def left(self,speed):
+           self.motion = "left"
+
+        def right(self,speed):
+           self.motion = "right"
+
+        def stop(self):
+           self.motion = None
+
+        def integrate_mcs(self,t):
+            x = self.mcs.x + t.x*math.cos(self.mcs.a) - t.y*math.sin(self.mcs.a)
+            y = self.mcs.y + t.x*math.sin(self.mcs.a) + t.y*math.cos(self.mcs.a)
+            a = self.mcs.a + t.a
+            return Coordinate(x,y,a)
+
+        def calc_t(self):
+            diff = getMs() - self.mcs_t #-- calculate difference to last timestamp
+            diff_s = diff/1000.
+
+            #-- calculate movement of bot
+            t = Coordinate()
+            if self.motion == "forward":
+                t.x = diff_s * self.config.getfloat("MOT", "MPERS50")
+            elif self.motion == "backward":
+                t.x = -diff_s * self.config.getfloat("MOT", "MPERS50")
+            elif self.motion == "left":
+                t.a = diff_s * self.config.getfloat("MOT", "APERS50")
+            elif self.motion == "right":
+                t.a = -diff_s * self.config.getfloat("MOT", "APERS50")
+
+            return t
+            
+
+        def run_impl(self):
+            # integrate motion to perform dead-reckoning
+            self.mcs = self.integrate_mcs(self.calc_t())
+            self.mcs_t = getMs() #-- get current time in milliseconds
+
+            if "Move" in self.md:
+                if len(self.md["Move"]) == 2:
+                    #-- get the command
+                    print self.name, " - MOVING!"
+                    speed = self.md["Move"][0]
+                    direction = self.md["Move"][1]
+                    #-- take it out of the queue
+                    self.md["Move"] = []
+                    #-- execute
+                    if direction == "forward":
+                        print "Motor: FWD"
+                        self.fwd(speed)
+                    elif direction == "backward":
+                        print "Motor: BWD"
+                        self.bwd(speed)
+                    elif direction == "left":
+                        print "Motor: LEFT"
+                        self.left(speed)
+                    elif direction == "right":
+                        print "Motor: RIGHT"
+                        self.right(speed)
+
+                    time.sleep(1)
+                    self.stop()
+                    print self.name, " - MOVING DONE!"
+
+        def cleanup(self):
+            pass
+
--- code/framework/modules/US.py
+++ code/framework/modules/US.py
@@ -1,38 +1,54 @@
 from MP import MP
-import RPi.GPIO as GPIO
 import time
 
-class US(MP):
-    def init(self):
-        self.d = -1
+try:
+    import RPi.GPIO as GPIO
 
-        GPIO.setmode(GPIO.BOARD)
-        self.t = self.config.getint("PINS", "US1T")
-        self.e = self.config.getint("PINS", "US1E")
+    class US(MP):
+        def init(self):
+            self.d = -1
 
-        GPIO.setup(self.t, GPIO.OUT)
-        GPIO.setup(self.e, GPIO.IN)
+            GPIO.setmode(GPIO.BOARD)
+            self.t = self.config.getint("PINS", "US1T")
+            self.e = self.config.getint("PINS", "US1E")
 
-        GPIO.output(self.t, False)
+            GPIO.setup(self.t, GPIO.OUT)
+            GPIO.setup(self.e, GPIO.IN)
 
-    def measure(self):
-        st = 0
-        et = 0
+            GPIO.output(self.t, False)
 
-        GPIO.output(self.t, 1)
-        time.sleep(0.00001)
-        GPIO.output(self.t, 0)
+        def measure(self):
+            st = 0
+            et = 0
 
-        while GPIO.input(self.e) == 0:
-            st = time.time()
+            GPIO.output(self.t, 1)
+            time.sleep(0.00001)
+            GPIO.output(self.t, 0)
 
-        while GPIO.input(self.e) == 1:
-            et = time.time()
+            while GPIO.input(self.e) == 0:
+                st = time.time()
 
-        return ((et-st) * 34300) / 2
+            while GPIO.input(self.e) == 1:
+                et = time.time()
 
-    def run_impl(self):
-        pass
-        #d = self.measure()
-        #print self.name ," - Distance: ", d
-        #self.md["US1"] = d
+            return ((et-st) * 34300) / 2
+
+        def run_impl(self):
+            pass
+            #d = self.measure()
+            #print self.name ," - Distance: ", d
+            #self.md["US1"] = d
+except: # not on raspberry pi, dryrun
+    class US(MP):
+        def init(self):
+            self.d = -1
+
+        def measure(self):
+            pass
+
+        def run_impl(self):
+            pass
+            #d = self.measure()
+            #print self.name ," - Distance: ", d
+            #self.md["US1"] = d
+
